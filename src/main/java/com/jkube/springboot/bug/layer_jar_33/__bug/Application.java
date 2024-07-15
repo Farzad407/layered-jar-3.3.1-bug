4d50bd89179e73b47be40b1dff479fdd0d8e64eb
Farzad Kohantorabi <fkohantorabi@407etr.com>
2024-07-15 15:35:39 -0400

    sample project

---
+++ src/main/java/com/jkube/springboot/bug/layer_jar_33/__bug/Application.java
@@ -0,0 +1,13 @@
+package com.jkube.springboot.bug.layer_jar_33.__bug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkube/springboot/bug/layer_jar_33/__bug/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkube.springboot.bug.layer_jar_33.__bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
